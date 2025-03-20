ee20146f3387ff90ee6581cc274dc617becdc757
kzenker <katja.zenker@teskalabs.com>
2025-03-20 18:50:31 +0100

    Translate invalid date

--- src/components/DateTime/InvalidDate.jsx
+++ src/components/DateTime/InvalidDate.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 // Mehod for rendering invalid date component
 export default function InvalidDate() {
+	const { t } = useTranslation();
 	return (
 		<span className='datetime text-nowrap'>
 			<i className='bi bi-clock pe-1' />
-			Invalid Date
+			{t('General|Invalid Date')}
 		</span>
 	);
 }
