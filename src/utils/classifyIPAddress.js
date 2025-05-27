89b7c1baa35ba79f8dcdb15e4fcd8867c393b39f
Pe5h4 <pesekf@email.cz>
2025-05-27 11:07:56 +0200

    Change structure

--- src/utils/classifyIPAddress.js
+++ src/utils/classifyIPAddress.js
@@ -36,87 +36,159 @@ const ipv4Ranges = [
 	{
 		block: parseIPv4("10.0.0.0"),
 		mask: 8,
-		class: "IPv4 private"
+		attrs: {
+			cls: "private",
+			family: "IPv4",
+		}
 	},
 	{
 		block: parseIPv4("172.16.0.0"),
 		mask: 12,
-		class: "IPv4 private"  // https://datatracker.ietf.org/doc/html/rfc1918
+		// https://datatracker.ietf.org/doc/html/rfc1918
+		attrs: {
+			cls: "private",
+			family: "IPv4",
+		}
 	},
 	{
 		block: parseIPv4("192.168.0.0"),
 		mask: 16,
-		class: "IPv4 private"
+		attrs: {
+			cls: "private",
+			family: "IPv4",
+		}
 	},
 	{
 		block: parseIPv4("169.254.0.0"),
 		mask: 24,
-		class: "IPv4 reserved"
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "reserved"
+		}
 	},
 	{
 		block: parseIPv4("169.254.255.0"),
 		mask: 24,
-		class: "IPv4 reserved"
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "reserved"
+		}
 	},
 	{
 		block: parseIPv4("169.254.0.0"),
 		mask: 16,
-		class: "IPv4 link-local"  // https://datatracker.ietf.org/doc/html/rfc3927
+		// https://datatracker.ietf.org/doc/html/rfc3927
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "link-local"
+		}
 	},
 	{
 		block: parseIPv4("100.64.0.0"),
 		mask: 10,
-		class: "IPv4 SAS"  // Shared Address Space  https://datatracker.ietf.org/doc/html/rfc6598
+		// Shared Address Space https://datatracker.ietf.org/doc/html/rfc6598
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "Shared Address Space"
+		}
 	},
 	{
 		block: parseIPv4("127.0.0.0"),
 		mask: 8,
-		class: "IPv4 loopback"
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "loopback"
+		}
 	},
 	{
 		block: parseIPv4("0.0.0.0"),
 		mask: 8,
-		class: "IPv4 special"
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+		}
 	},
 	{
 		block: parseIPv4("192.0.0.0"),
 		mask: 29,
-		class: "IPv4 ds-lite" // https://datatracker.ietf.org/doc/html/rfc6333
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "ds-lite"
+		}
 	},
 	{
 		block: parseIPv4("192.0.2.0"),
 		mask: 24,
-		class: "IPv4 documentation"  // https://datatracker.ietf.org/doc/html/rfc5737
+		// https://datatracker.ietf.org/doc/html/rfc5737
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "documentation"
+		}
 	},
 	{
 		block: parseIPv4("192.88.99.0"),
 		mask: 24,
-		class: "IPv4 6to4"  // RFC3068
+		// RFC3068
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "6to4"
+		}
 	},
 	{
 		block: parseIPv4("198.18.0.0"),
 		mask: 15,
-		class: "IPv4 benchmarking"  // https://datatracker.ietf.org/doc/html/rfc2544
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "benchmarking"
+		}
 	},
 	{
 		block: parseIPv4("198.51.100.0"),
 		mask: 24,
-		class: "IPv4 documentation"  // https://datatracker.ietf.org/doc/html/rfc5737
+		// https://datatracker.ietf.org/doc/html/rfc5737
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "documentation"
+		}
 	},
 	{
 		block: parseIPv4("203.0.113.0"),
 		mask: 24,
-		class: "IPv4 documentation"  // https://datatracker.ietf.org/doc/html/rfc5737
+		// https://datatracker.ietf.org/doc/html/rfc5737
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "documentation"
+		}
 	},
 	{
 		block: parseIPv4("240.0.0.0"),
 		mask: 4,
-		class: "IPv4 reserved" // [RFC1112], Section 4
+		// [RFC1112], Section 4
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "reserved"
+		}
 	},
 	{
 		block: parseIPv4("255.255.255.255"),
 		mask: 32,
-		class: "IPv4 limited-broadcast"  //  RFC0919], Section 7
+		attrs: {
+			cls: "special",
+			family: "IPv4",
+			msg: "limited-broadcast"
+		}
 	}	
 ]
 
@@ -124,62 +196,119 @@ const ipv6Ranges = [
 	{
 		block: parseIPv6("::1"),
 		mask: 128,
-		class: "IPv6 loopback"  // https://datatracker.ietf.org/doc/html/rfc4291
+		// https://datatracker.ietf.org/doc/html/rfc4291
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "loopback"
+		}
 	},
 	{
 		block: parseIPv6("::"),
 		mask: 128,
-		class: "IPv6 unspecified"  // https://datatracker.ietf.org/doc/html/rfc4291
+		// https://datatracker.ietf.org/doc/html/rfc4291
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "unspecified"
+		}
 	},
 	{
 		block: parseIPv6("64:ff9b::"),
 		mask: 96,
-		class: "IPv6 IPv4-IPv6"  // https://datatracker.ietf.org/doc/html/rfc6052
+		// https://datatracker.ietf.org/doc/html/rfc6052
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "IPv4-IPv6"
+		}
 	},
 	{
 		block: parseIPv6("::ffff:0:0"),
 		mask: 96,
-		class: "IPv6 IPv4-mapped"  // https://datatracker.ietf.org/doc/html/rfc4291
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "IPv4-mapped"
+		}
 	},
 	{
 		block: parseIPv6("100::"),
 		mask: 64,
-		class: "IPv6 discard"  // https://datatracker.ietf.org/doc/html/rfc6666
+		// https://datatracker.ietf.org/doc/html/rfc6666
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "discard"
+		}
 	},
 	{
 		block: parseIPv6("2001::"),
 		mask: 32,
-		class: "IPv6 terredo"  // https://datatracker.ietf.org/doc/html/rfc4380
+		// https://datatracker.ietf.org/doc/html/rfc4380
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "terredo"
+		}
 	},
 	{
 		block: parseIPv6("2001:2::"),
 		mask: 32,
-		class: "IPv6 benchmarking"  // https://datatracker.ietf.org/doc/html/rfc5180
+		// https://datatracker.ietf.org/doc/html/rfc5180
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "benchmarking"
+		}
 	},
 	{
 		block: parseIPv6("2001:db8::"),
 		mask: 32,
-		class: "IPv6 documentation"  // https://datatracker.ietf.org/doc/html/rfc3849
+		// https://datatracker.ietf.org/doc/html/rfc3849
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "documentation"
+		}
 	},
 	{
 		block: parseIPv6("2001:10::"),
 		mask: 28,
-		class: "IPv6 orchid"  // https://datatracker.ietf.org/doc/html/rfc4843
+		// https://datatracker.ietf.org/doc/html/rfc4843
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "orchid"
+		}
 	},
 	{
 		block: parseIPv6("2002::"),
 		mask: 16,
-		class: "IPv6 6to4"  // https://datatracker.ietf.org/doc/html/rfc3056
+		// https://datatracker.ietf.org/doc/html/rfc3056
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "6to4"
+		}
 	},
 	{
 		block: parseIPv6("fc00::"),
 		mask: 7,
-		class: "IPv6 private"  // https://datatracker.ietf.org/doc/html/rfc4193
+		// https://datatracker.ietf.org/doc/html/rfc4193
+		attrs: {
+			cls: "private",
+			family: "IPv6",
+		}
 	},
 	{
 		block: parseIPv6("fe80::"),
 		mask: 10,
-		class: "IPv6 link-local"  // https://datatracker.ietf.org/doc/html/rfc4291
+		attrs: {
+			cls: "special",
+			family: "IPv6",
+			msg: "link-local"
+		}
 	}
 ]
 
@@ -191,11 +320,15 @@ function classifyIPv4(ipAddress) {
 		const ipNetwork = ipAddress & ~mask;
 		
 		if (network === ipNetwork) {
-			return range.class;
+			return range.attrs;
 		}
 	}
 
-	return "IPv4 public";  // If no range matches, it's a public IP
+	return {
+		cls: "public",
+		family: "IPv4",
+		value: ipAddress,
+	}
 }
 
 function classifyIPv6(ipAddress) {
@@ -205,11 +338,15 @@ function classifyIPv6(ipAddress) {
 		const ipNetwork = ipAddress & ~mask;
 		
 		if (network === ipNetwork) {
-			return range.class;
+			return range.attrs;
 		}
 	}
 
-	return "IPv6 public";  // If no range matches, it's a public IP
+	return {
+		cls: "public",
+		family: "IPv6",
+		value: ipAddress,
+	}
 }
 
 
@@ -219,21 +356,7 @@ function classifyIPv6(ipAddress) {
 		ipAddress - string, IP address to classify
 	
 	Returns:
-		"IPv4 private"
-		"IPv4 public"
-		"IPv4 ..."
-		"IPv6 private"
-		"IPv6 public"
-		"IPv6 ..."
-		"invalid" ... for invalid IP address
-		"invalid parse ipv4" ... for invalid IPv4 address
-		"invalid parse ipv6" ... for invalid IPv6 address
-
-	The first word is "IPv4", "IPv6" or "invalid".
-	
-	If the second word is "private", it means that the IP address is private.
-	If the second word is "public", it means that the IP address is public.
-	Any other second word means "special".
+		Dictionary
 */
 
 export function classifyIPAddress(ipAddress) {
@@ -244,7 +367,11 @@ export function classifyIPAddress(ipAddress) {
 	try {
 		ipv4 = parseIPv4(ipAddress);
 	} catch (error) {
-		return "invalid parse ipv4";
+		return {
+			cls: "invalid",
+			value: ipAddress,
+			msg:"invalid parse ipv4",
+		};
 	}
 	if (ipv4 !== undefined) {
 		return classifyIPv4(ipv4);
@@ -254,11 +381,18 @@ export function classifyIPAddress(ipAddress) {
 	try {
 		ipv6 = parseIPv6(ipAddress);
 	} catch (error) {
-		return "invalid parse ipv6";
+		return {
+			cls: "invalid",
+			value: ipAddress,
+			msg:"invalid parse ipv6",
+		};
 	}
 	if (ipv6 !== undefined) {
 		return classifyIPv6(ipv6);
 	}
 
-	return "invalid";  // Means invalid IP address
+	return {
+		cls: "invalid",
+		value: ipAddress,
+	};
 }
