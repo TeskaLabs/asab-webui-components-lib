6cfd015aa947c36d8257b44ac4d63f62b8d80434
JanMajer86 <majer.jan.1986@gmail.com>
2025-06-16 16:35:25 +0200

    create translation utility

---
+++ src/utils/translateFromContent.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+
+export function translateFromContent(content) {
+	const currentLang = (i18n?.language || '').toLowerCase();
+
+	if (typeof content === 'string') {
+		return content;
+	}
+
+	if (!content || typeof content !== 'object' || Array.isArray(content)) {
+		return '';
+	}
+
+	const normalizedKeys = Object.keys(content).reduce((acc, key) => {
+		acc[key.toLowerCase()] = content[key];
+		return acc;
+	}, {});
+
+	return (
+		normalizedKeys[currentLang] || normalizedKeys['c'] || Object.values(normalizedKeys)[0] || ''
+	);
+}
