d464070d1e79f2dee76dba0fc8d22c97da1c909a
JanMajer86 <majer.jan.1986@gmail.com>
2025-06-16 11:35:41 +0200

    quotes fix

--- src/components/DateTime/absolute/getFormattedTime.js
+++ src/components/DateTime/absolute/getFormattedTime.js
@@ -1,6 +1,6 @@
 import { timeToStringCommon } from '../utils/timeToStringCommon.jsx';
 
-// Absolute time formatting with "ago"
+// Absolute time formatting with 'ago'
 export const getFormattedTime = (value, dateTimeFormat = 'medium', locale = undefined) => {
 	const { date, distanceToNow } = timeToStringCommon(value, dateTimeFormat, locale, true); // addSuffix = true
 	if (date === 'Invalid Date') {
