bafc6e09d798c53904b7deac6d9925208bfd3821
JanMajer86 <majer.jan.1986@gmail.com>
2025-06-16 11:24:20 +0200

    centralize date/time default values in utils/timeFormatDefaults.js

--- src/components/DateTime/absolute/timeToString.js
+++ src/components/DateTime/absolute/timeToString.js
@@ -1,7 +1,8 @@
 import getFormattedTime from './getFormattedTime.js';
+import { DEFAULT_DATE_FORMAT, DEFAULT_LOCALE} from '../utils/timeFormatDefaults.js'
 
 // Simplifies original timeToString
-export const timeToString = (value, dateTimeFormat = 'medium', locale= undefined) => {
+export const timeToString = (value, dateTimeFormat = DEFAULT_DATE_FORMAT, locale= DEFAULT_LOCALE) => {
 	const result = getFormattedTime(value, dateTimeFormat, locale);
 	return result.date;
 }
--- src/components/DateTime/relative/timeToStringRelative.jsx
+++ src/components/DateTime/relative/timeToStringRelative.jsx
@@ -1,7 +1,8 @@
 import { getFormattedTimeRelative } from './getFormattedTimeRelative.jsx';
+import { DEFAULT_DATE_FORMAT, DEFAULT_LOCALE} from '../utils/timeFormatDefaults.js'
 
 // Simplifies original timeToStringRelative
-export const timeToStringRelative = (value, dateTimeFormat = 'medium', addSuffix = false, locale = undefined) => {
+export const timeToStringRelative = (value, dateTimeFormat = DEFAULT_DATE_FORMAT, addSuffix = false, locale = DEFAULT_LOCALE) => {
 	const result = getFormattedTimeRelative(value, dateTimeFormat, addSuffix, locale);
 	return result.date
 }
