72ea88bea784832b0d2bdd25306baa3ddaf2bd73
JanMajer86 <majer.jan.1986@gmail.com>
2025-06-17 17:30:00 +0200

    testing

--- src/utils/translateFromContent.js
+++ src/utils/translateFromContent.js
@@ -2,6 +2,9 @@ import i18n from 'i18next';
 
 export function translateFromContent(content) {
 	const currentLang = (i18n?.language || '').toLowerCase();
+	// console.log(i18n.language);
+	// const currentLang = 'de';
+	// console.log(currentLang)
 
 	if (typeof content === 'string') {
 		return content;
