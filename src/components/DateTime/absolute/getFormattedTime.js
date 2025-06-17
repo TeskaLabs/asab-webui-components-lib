1715beae0370223e6269e0a01d1140a5484d2f12
JanMajer86 <majer.jan.1986@gmail.com>
2025-06-17 14:11:26 +0200

    import default values

--- src/components/DateTime/absolute/getFormattedTime.js
+++ src/components/DateTime/absolute/getFormattedTime.js
@@ -1,7 +1,8 @@
 import { timeToStringCommon } from '../utils/timeToStringCommon.jsx';
+import { DEFAULT_DATE_FORMAT, DEFAULT_LOCALE } from '../utils/timeFormatDefaults.js'
 
 // Absolute time formatting with 'ago'
-export const getFormattedTime = (value, dateTimeFormat = 'medium', locale = undefined) => {
+export const getFormattedTime = (value, dateTimeFormat = DEFAULT_DATE_FORMAT, locale = DEFAULT_LOCALE) => {
 	const { date, distanceToNow } = timeToStringCommon(value, dateTimeFormat, locale, true); // addSuffix = true
 	if (date === 'Invalid Date') {
 		return { date };
--- src/components/DateTime/relative/getFormattedTimeRelative.jsx
+++ src/components/DateTime/relative/getFormattedTimeRelative.jsx
@@ -1,7 +1,8 @@
 import { timeToStringCommon } from '../utils/timeToStringCommon.jsx';
+import { DEFAULT_DATE_FORMAT, DEFAULT_LOCALE } from '../utils/timeFormatDefaults.js'
 
 // Relative time formatting without 'ago'
-export const getFormattedTimeRelative = (value, dateTimeFormat = 'medium', addSuffix = false, locale = undefined) => {
+export const getFormattedTimeRelative = (value, dateTimeFormat = DEFAULT_DATE_FORMAT, addSuffix = false, locale = DEFAULT_LOCALE) => {
 	// TODO: add addSuffix = true for values which are to the future from now (we can use of isAfter from date-fns)
 	const { date, distanceToNow } = timeToStringCommon(value, dateTimeFormat, locale, addSuffix);
 	if (date === 'Invalid Date') {
