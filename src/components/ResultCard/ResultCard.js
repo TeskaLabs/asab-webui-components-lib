666ee442d6b344e11c8037776903cfa2ab252300
akonshina <arina.konshina@teskalabs.com>
2025-07-02 07:59:30 +0200

    add padding

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -8,7 +8,7 @@ export function ResultCard({ body, isSuccessful = true }) {
 
 	return (
 		<Card className='result-card'>
-			<CardBody className='text-center d-flex align-items-center justify-content-center'>
+			<CardBody className='text-center d-flex align-items-center justify-content-center pb-4'>
 				<div className='justify-content-center'>
 					<i className={`pe-2 mb-3 fs-1 bi ${(isSuccessful == true) ? 'bi-check-circle-fill text-success' : 'bi-exclamation-triangle-fill text-danger'}`}></i>
 					<div>
