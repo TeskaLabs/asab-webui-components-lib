9ff030c414d511ea7b7928f3c1e7d224302abb34
Pe5h4 <pesekf@email.cz>
2025-07-21 11:49:54 +0200

    Update reducer registry

--- src/components/Context/store/reducer/reducerRegistry.jsx
+++ src/components/Context/store/reducer/reducerRegistry.jsx
@@ -2,7 +2,12 @@
 const reducers = {};
 const initialStates = {};
 
-export function registerReducer(key, reducer, initialState) {
+export function registerReducer(key, reducer) {
+	/*
+		The initial state is retrieved by calling the reducer with an undefined state
+		This can be handled because of default `state = initialState` pattern in the reducers
+	*/
+	const initialState = reducer(undefined, { type: '@@ASAB_APP_STORE/INIT_STATE' }); // @@ASAB_APP_STORE/INIT_STATE type used to avoid any potential reducer type collision
 	reducers[key] = reducer;
 	initialStates[key] = initialState;
 }
