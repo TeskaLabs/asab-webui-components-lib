cee726d9e3021827299fa071f7adafd017373d13
JanMajer86 <majer.jan.1986@gmail.com>
2025-08-12 15:01:25 +0200

    implementing results card extension (hande more states)

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -4,13 +4,26 @@ import { Card, CardBody } from 'reactstrap';
 
 import './ResultCard.scss';
 
-export function ResultCard({ body, isSuccessful = true }) {
+export function ResultCard({ body, status = 'success' }) {
+
+	const getIconAndColor = () => {
+		switch (status) {
+			case 'success':
+				return 'bi-check-circle-fill text-success';
+			case 'warning':
+				return 'bi-cone-striped text-warning';
+			case 'error':
+				return 'bi-exclamation-triangle-fill text-danger';
+			default:
+				return 'bi-check-circle-fill text-success';
+		}
+	};
 
 	return (
 		<Card className='result-card'>
 			<CardBody className='text-center d-flex align-items-center justify-content-center'>
 				<div className='justify-content-center pb-2'>
-					<i className={`pe-2 mb-3 fs-1 bi ${(isSuccessful == true) ? 'bi-check-circle-fill text-success' : 'bi-exclamation-triangle-fill text-danger'}`}></i>
+					<i className={`pe-2 mb-3 fs-1 bi ${getIconAndColor()}`}></i>
 					<div>
 						{body}
 					</div>
