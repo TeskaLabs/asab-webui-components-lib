3922c5c5611c7a1ce73d12de569b3da9e00c5484
akonshina <arina.konshina@teskalabs.com>
2025-08-19 10:15:03 +0200

    follow custompill in custom filter

--- src/components/DataTable2/components/filters/DataTableAdvCustomFilter.jsx
+++ src/components/DataTable2/components/filters/DataTableAdvCustomFilter.jsx
@@ -11,8 +11,7 @@ export function DataTableAdvCustomFilter({ field, content, customPill }) {
 		setFilterField(field);
 		const fieldKey = Object.keys(field)[0];
 		setCustomPill(customPill, fieldKey);
-
-	},[]);
+	},[customPill]);
 
 	// Transferring a function to the custom content for correct filter assignment
 	return content(updateSingleValueFilter);
