370f26120653cbf13cfb71282805242b585dbb2d
JanMajer86 <majer.jan.1986@gmail.com>
2025-10-13 11:39:32 +0200

    "google" spinner refactor

--- src/components/Spinner2/Spinner2.jsx
+++ src/components/Spinner2/Spinner2.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Row } from "reactstrap";
+import './Spinner2.scss';
 
 export function Spinner2() {
 	return (
 		<Row className="loading-spinner-div justify-content-center">
-			<div className="suspense-loader spinner-grow text-primary mx-2" role="status">
-			</div>
-			<div className="suspense-loader spinner-grow text-primary mx-2" role="status">
-				<span className="visually-hidden">Loading...</span>
-			</div>
-			<div className="suspense-loader spinner-grow text-primary mx-2" role="status">
+			<div class="spinner">
+				<svg class="circular" viewBox="25 25 50 50">
+					<circle
+						class="path"
+						cx="50"
+						cy="50"
+						r="20"
+						fill="none"
+						stroke-width="5"
+						stroke-miterlimit="10"
+					/>
+				</svg>
 			</div>
 		</Row>
 	);
