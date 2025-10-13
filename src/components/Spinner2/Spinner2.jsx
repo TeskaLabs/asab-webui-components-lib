7d023748727124355b80ed4b701c1d0f7c2d1995
JanMajer86 <majer.jan.1986@gmail.com>
2025-10-13 13:12:03 +0200

    add custom color, size props

--- src/components/Spinner2/Spinner2.jsx
+++ src/components/Spinner2/Spinner2.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Row } from "reactstrap";
+import { Row } from 'reactstrap';
 import './Spinner2.scss';
 
-export function Spinner2() {
+export function Spinner2({ color = 'primary', size = 50 }) {
+	const colorClass = `spinner-${color}`;
+	const sizeStyle = { width: `${size}px`, height: `${size}px` };
+	
 	return (
-		<Row className="loading-spinner-div justify-content-center">
-			<div class="spinner">
-				<svg class="circular" viewBox="25 25 50 50">
+		<Row className='loading-spinner-div justify-content-center'>
+			<div className={`spinner ${colorClass}`} style={sizeStyle}>
+				<svg className='circular' viewBox='25 25 50 50'>
 					<circle
-						class="path"
-						cx="50"
-						cy="50"
-						r="20"
-						fill="none"
-						stroke-width="5"
-						stroke-miterlimit="10"
+						className='path'
+						cx='50'
+						cy='50'
+						r='20'
+						fill='none'
+						strokeWidth='5'
+						strokeMiterlimit='10'
 					/>
 				</svg>
 			</div>
