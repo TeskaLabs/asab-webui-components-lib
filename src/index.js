ef599a4e5bf1a9ff91cebd003a0cbc9d22953193
JanMajer86 <majer.jan.1986@gmail.com>
2026-08-11 11:43:50 +0200

    remove legacy DataTable and corresponding exports

--- src/index.js
+++ src/index.js
@@ -46,7 +46,3 @@ export { AppStoreProvider, useAppStore, useAppSelector, createAppStore } from '.
 export { registerReducer } from './components/Context/store/reducer/reducerRegistry.jsx';
 export { CopyableInput } from './components/CopyableInput/CopyableInput';
 export { setBacklink, setBacklinkFromLocation, getBacklink, getBacklinkData, clearBacklink, consumeBacklink, consumeBacklinkData, createBacklinkHandlers } from './utils/backlink/backlink.jsx';
-
-// OBSOLETED COMPONENTS (Aug 2023) - don't use this in a new designs
-export { default as Pagination } from './components/DataTable/Pagination';
-export { DataTable } from './components/DataTable';
